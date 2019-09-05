dcb6c91c766eada746fca599296b9da57801337c
whatsrupp <nickrupp95@gmail.com>
2019-09-05 22:48:15 +0100

    add audio record and playback test button

--- src/scenes/loops/Loops.tsx
+++ src/scenes/loops/Loops.tsx
@@ -2,23 +2,64 @@ import React from "react";
 
 import * as SC from "./styled";
 import * as images from "./assets/images";
+import Track from "./Track";
+import AudioRecorder from "./AudioRecorder";
 
-const Loops: React.FC = () => {
-  return (
-    <SC.PageLayout>
-      <SC.Header>
-        <SC.Head src={images.jack} />
-        <SC.Head src={images.nick} />
-        <SC.Logo src={images.logo} />
-        <SC.Head src={images.will} />
-        <SC.Head src={images.viv} />
-      </SC.Header>
-      <SC.Body></SC.Body>
-      <SC.Footer>
-        <SC.RecordButton />
-      </SC.Footer>
-    </SC.PageLayout>
-  );
-};
+interface ILoopsProps {}
+interface ILoopsState {
+  isRecording: boolean;
+  AudioContext: any;
+  audioUrl: string;
+}
+
+class Loops extends React.Component<ILoopsProps, ILoopsState> {
+  constructor(props: ILoopsProps) {
+    super(props);
+
+    this.state = {
+      isRecording: false,
+      AudioContext: null,
+      audioUrl: ""
+    };
+  }
+  componentDidMount = async () => {
+    const AudioContext = await AudioRecorder();
+    this.setState({ AudioContext });
+  };
+
+  onRecordClick = async () => {
+    const { isRecording, AudioContext } = this.state;
+
+    if (isRecording) {
+      AudioContext.stop((audioUrl: string) => {
+        this.setState({ audioUrl });
+      });
+    } else {
+      AudioContext.start();
+    }
+    this.setState({ isRecording: !isRecording });
+  };
+
+  render() {
+    return (
+      <SC.PageLayout>
+        <SC.Header>
+          <SC.Head src={images.jack} />
+          <SC.Head src={images.nick} />
+          <SC.Logo src={images.logo} />
+          <SC.Head src={images.will} />
+          <SC.Head src={images.viv} />
+        </SC.Header>
+        <SC.Body>
+          <Track />
+          <audio src={this.state.audioUrl} controls />
+        </SC.Body>
+        <SC.Footer>
+          <SC.RecordButton onClick={this.onRecordClick} />
+        </SC.Footer>
+      </SC.PageLayout>
+    );
+  }
+}
 
 export default Loops;
---
+++ src/scenes/loops/Track.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import * as SC from "./styled";
+import * as images from "./assets/images";
+
+const Track: React.FC = () => {
+  return <SC.Track />;
+};
+
+export default Track;
