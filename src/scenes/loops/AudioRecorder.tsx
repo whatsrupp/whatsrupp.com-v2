077ff7d33fb805e595e22829e67a896dc549b899
whatsrupp <nickrupp95@gmail.com>
2019-09-09 23:05:00 +0100

    Create React component with audio recorder

---
+++ src/scenes/loops/AudioRecorder.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+interface IAudioRecorderProps {
+  render: any;
+}
+interface IAudioRecorderState {
+  isRecording: boolean;
+  AudioRecorder: any;
+  audioUrl: string;
+  audioChunks: any;
+}
+
+declare const MediaRecorder: any;
+
+class AudioRecorder extends React.Component<
+  IAudioRecorderProps,
+  IAudioRecorderState
+> {
+  constructor(props: IAudioRecorderProps) {
+    super(props);
+    this.state = {
+      AudioRecorder: null,
+      audioChunks: [],
+      audioUrl: "",
+      isRecording: false
+    };
+  }
+
+  componentDidMount = async () => {
+    const audioStream = await navigator.mediaDevices.getUserMedia({
+      audio: true
+    });
+
+    const AudioRecorder = new MediaRecorder(audioStream);
+    this.setState({ AudioRecorder });
+
+    AudioRecorder.addEventListener("dataavailable", (event: { data: any }) => {
+      const newAudioChunks = [...this.state.audioChunks, event.data];
+      this.setState({ audioChunks: newAudioChunks });
+    });
+
+    AudioRecorder.addEventListener("stop", () => {
+      const audioBlob = new Blob(this.state.audioChunks);
+      const audioUrl = URL.createObjectURL(audioBlob);
+      this.setState({ audioUrl });
+    });
+  };
+
+  startRecording = () => {
+    const { AudioRecorder } = this.state;
+    this.setState({ audioChunks: [] });
+    AudioRecorder.start();
+    this.setState({ isRecording: true });
+  };
+
+  stopRecording = () => {
+    const { AudioRecorder } = this.state;
+    this.setState({ isRecording: false });
+    AudioRecorder.stop();
+  };
+
+  render() {
+    return this.props.render({
+      audioUrl: this.state.audioUrl,
+      isRecording: this.state.isRecording,
+      stopRecording: this.stopRecording,
+      startRecording: this.startRecording
+    });
+  }
+}
+
+export default AudioRecorder;
