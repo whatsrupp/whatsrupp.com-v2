83cd4de75c125862039dc0b2ef586edeaeb321cf
whatsrupp <nickrupp95@gmail.com>
2019-09-23 14:29:10 +0100

    update aesthetic of landing page

--- src/scenes/landing/Landing.tsx
+++ src/scenes/landing/Landing.tsx
@@ -1,34 +1,49 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
 import * as SC from "./styled";
 import icons from "../../style/icons";
 import NathanFace from "./assets/images/NathanFace.png";
 import LoopsLogo from "./assets/images/LoopsLogo.png";
+import ReactPageScroller from "react-page-scroller";
 
 const Landing: React.FC = () => {
+  const reactPageScroller = useRef();
+
   return (
     <SC.PageLayout>
-      <SC.PageContent>
-        <SC.Header></SC.Header>
-        <SC.Body>
-          <SC.Row to={"/soundboard"}>
-            <SC.RowHeader>King Soundboard</SC.RowHeader>
-            <SC.RowPicture src={NathanFace} />
-            <SC.RowText>A Soundboard made to annoy my housemate</SC.RowText>
-          </SC.Row>
-          <SC.Row to={"/portfolio"}>
-            <SC.RowHeader>whatsrupp.com v 1.0.0</SC.RowHeader>
+      <ReactPageScroller ref={reactPageScroller}>
+        <SC.FullPage>
+          <SC.PortfolioRow to={"/portfolio"}>
             <SC.RowPicture src={icons.palette} />
-            <SC.RowText>The original</SC.RowText>
-          </SC.Row>
-          <SC.Row to={"/loops"}>
-            <SC.RowHeader>Loops and Cats</SC.RowHeader>
+            <SC.Information>
+              <SC.RowHeader>whatsrupp.com v 1.0.0</SC.RowHeader>
+              <SC.RowText>
+                My first attempt at a portfolio website back in 2017
+              </SC.RowText>
+            </SC.Information>
+          </SC.PortfolioRow>
+        </SC.FullPage>
+        <SC.FullPage>
+          <SC.SoundboardRow to={"/soundboard"}>
+            <SC.RowPicture src={NathanFace} />
+            <SC.Information>
+              <SC.RowHeader>King Soundboard</SC.RowHeader>
+              <SC.RowText>A Soundboard made to annoy my housemate</SC.RowText>
+            </SC.Information>
+          </SC.SoundboardRow>
+        </SC.FullPage>
+        <SC.FullPage>
+          <SC.LoopsRow to={"/loops"}>
             <SC.RowPicture src={LoopsLogo} />
-            <SC.RowText>A Live Looping Recording Station</SC.RowText>
-          </SC.Row>
-        </SC.Body>
-        <SC.Footer></SC.Footer>>
-      </SC.PageContent>
+            <SC.Information>
+              <SC.RowHeader>Loops and Cats</SC.RowHeader>>
+              <SC.RowText>
+                A Live Looping Recording Station - Currently Under Construction
+              </SC.RowText>
+            </SC.Information>
+          </SC.LoopsRow>
+        </SC.FullPage>
+      </ReactPageScroller>
     </SC.PageLayout>
   );
 };
--- src/scenes/landing/styled.ts
+++ src/scenes/landing/styled.ts
@@ -4,16 +4,20 @@ import * as colours from "../../style/colours";
 import components from "../../style/components";
 import { Link } from "react-router-dom";
 export const PageLayout = styled(PageLayoutComponent)`
-  background: ${colours.purple};
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+
   overflow: scroll;
 
   * {
     font-family: "Montserrat", sans-serif;
   }
+`;
+
+export const FullPage = styled.div`
+  height: 100%;
+  width: 100%;
 `;
 
 export const PageContent = styled(components.PageLayout)`
@@ -51,37 +55,82 @@ export const Body = styled.div`
 `;
 
 export const Row = styled(Link)`
-  transition: all 0.4s ease-in-out;
   text-decoration: none;
-  display: grid;
-  justify-items: center;
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
-  grid-template-columns: 1fr 5fr;
-  grid-template-rows: 1fr 2fr;
+
   grid-gap: 10px;
   padding: 10px;
+  width: 100%;
+  height: 100%;
   border-radius: 5px;
-  color: ${colours.white};
   :hover {
-    transform: scale(1.01);
     cursor: pointer;
-    background-color: ${colours.orange};
-
-    box-shadow: 0 0 5px 10px ${colours.orange};
   }
   grid-template-areas:
-    "picture header"
-    "picture text";
+    "picture"
+    "header"
+    "text";
+`;
+
+export const SoundboardRow = styled(Row)`
+  background: #093028; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to right,
+    #237a57,
+    #093028
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to right,
+    #237a57,
+    #093028
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+`;
+
+export const PortfolioRow = styled(Row)`
+  background: #2c3e50; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to right,
+    #4ca1af,
+    #2c3e50
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to right,
+    #4ca1af,
+    #2c3e50
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+`;
+
+export const LoopsRow = styled(Row)`
+  background: #0f2027; /* fallback for old browsers */
+  background: linear-gradient(
+    to right,
+    #2c5364,
+    #203a43,
+    #0f2027
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 `;
 
 export const RowPicture = styled.img`
-  grid-area: picture;
   height: 100px;
+  z-index: 0;
+`;
+
+export const Information = styled.div`
+  position: absolute;
+  bottom: 5%;
+  left: 5%;
 `;
 
 export const RowHeader = styled.h1`
   grid-area: header;
   font-size: 1.4em;
+  color: ${colours.white};
+  text-align: left;
+
   font-weight: bold;
   justify-self: left;
 `;
@@ -89,6 +138,46 @@ export const RowHeader = styled.h1`
 export const RowText = styled.p`
   grid-area: text;
   justify-self: left;
+  text-align: left;
+  color: ${colours.white};
 `;
 
 export const Footer = styled.div``;
+
+export const PulsatingCircle = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translateX(-50%) translateY(-50%);
+  width: 50px;
+  height: 50px;
+  
+  &:before {
+    content: '';
+    position: relative;
+    display: block;
+    width: 300%;
+    height: 300%;
+    box-sizing: border-box;
+    margin-left: -100%;
+    margin-top: -100%;
+    border-radius: 100px;
+    background-color: #01a4e9;
+    animation: pulse-ring 1.25s cubic-bezier(0.215, 0.61, 0.355, 1);
+    animation-fill-mode: forwards;
+    animation-delay: 3s;
+
+  }
+  
+}
+
+@keyframes pulse-ring {
+  0% {
+    transform: scale(.33);
+  }
+  80%, 100% {
+    opacity: 0;
+  }
+}
+
+`;
