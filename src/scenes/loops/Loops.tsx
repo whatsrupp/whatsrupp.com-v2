11866dfaf12044cebbcf2f100c6023d060388bd6
whatsrupp <nickrupp95@gmail.com>
2019-10-08 22:36:44 +0100

    remove old provider definition

--- src/scenes/loops/Loops.tsx
+++ src/scenes/loops/Loops.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import * as SC from "./styled";
 import * as images from "./assets/images";
@@ -6,67 +6,28 @@ import Track from "./Track";
 import AudioRecorder from "./WithAudioRecorder";
 import Metronome from "./Metronome";
 import MetronomeButton from "./MetronomeButton";
-import { TimingContextValuesType } from "./types";
-
-import createContext from "./createContext";
-
-export const [useTimingContext, TimingContextProvider] = createContext<
-  TimingContextValuesType
->();
 
 const Loops: React.FC = () => {
-  const [AudioContext, setAudioContext] = useState(null);
-
-  useEffect(() => {
-    if (!AudioContext) {
-      setAudioContext(new window.AudioContext());
-    } else {
-      const cleanup = () => {
-        AudioContext.close();
-      };
-      return cleanup;
-    }
-  }, [AudioContext]);
-
-  if (!AudioContext) {
-    return <div>loading</div>;
-  }
   return (
     <Metronome>
-      {(args: any) => {
-        const { audioContext, createCue, removeCue } = args;
-
-        return (
-          <TimingContextProvider
-            value={{
-              AudioContext: audioContext,
-              createCue,
-              removeCue,
-              tempo: 120,
-              beatsInBar: 4
+      <SC.PageLayout>
+        <SC.Header>
+          <SC.Head src={images.jack} />
+          <SC.Head src={images.nick} />
+          <SC.Logo src={images.logo} />
+          <SC.Head src={images.will} />
+          <SC.Head src={images.viv} />
+        </SC.Header>
+        <SC.Body>
+          <AudioRecorder>
+            {(props: any) => {
+              return <Track {...props} />;
             }}
-          >
-            <SC.PageLayout>
-              <SC.Header>
-                <SC.Head src={images.jack} />
-                <SC.Head src={images.nick} />
-                <SC.Logo src={images.logo} />
-                <SC.Head src={images.will} />
-                <SC.Head src={images.viv} />
-              </SC.Header>
-              <SC.Body>
-                <AudioRecorder>
-                  {(props: any) => {
-                    return <Track {...props} />;
-                  }}
-                </AudioRecorder>
-                <MetronomeButton />
-              </SC.Body>
-              <SC.Footer></SC.Footer>
-            </SC.PageLayout>
-          </TimingContextProvider>
-        );
-      }}
+          </AudioRecorder>
+          <MetronomeButton />
+        </SC.Body>
+        <SC.Footer></SC.Footer>
+      </SC.PageLayout>
     </Metronome>
   );
 };
