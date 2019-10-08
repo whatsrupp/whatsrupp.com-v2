93a4adf135749989364a684f58202df52a6e1ff9
whatsrupp <nickrupp95@gmail.com>
2019-10-08 22:33:14 +0100

    move metronome types to a different file

---
+++ src/scenes/loops/Metronome/types.ts
@@ -0,0 +1,60 @@
+export type cueCallbackArguments = {
+  audioContext: any;
+  currentBeat: number;
+  currentBar: number;
+  currentBeatTime: number;
+  cueFunctionAtTime: (callbackFunction: Function, time: number) => void;
+  tempo: number;
+  beatsInBar: number;
+};
+
+export type createCue = (createCueArguments: {
+  cueCallback: (cueCallbackArguments: cueCallbackArguments) => void;
+  isRecurring: boolean;
+}) => number;
+
+export type TimingContextValuesType = {
+  AudioContext: any;
+  tempo: number;
+  beatsInBar: number;
+  createCue: createCue;
+  removeCue: (cueId: number) => void;
+};
+
+export type MetronomeContext = {
+  AudioContext: any;
+  tempo: number;
+  beatsInBar: number;
+  createCue: createCue;
+  removeCue: (cueId: number) => void;
+};
+
+export type cueDefinitionType = {
+  cueCallback: (args: {
+    cueCallbackArguments: cueCallbackArguments;
+    isRecurring: boolean;
+  }) => void;
+  isRecurring: Boolean;
+};
+
+export type MetronomeProps = {
+  tempo: number;
+  beatsInBar: number;
+  numberOfBars: number;
+  children: React.ReactNode;
+};
+
+export type MetronomeState = {
+  isPlaying: boolean;
+  beat: number;
+  bar: number;
+};
+
+type MetronomeRenderProps = {
+  audioContext: any;
+  createCue: (cueDefinition: cueDefinitionType) => number;
+  removeCue: (cueId: number) => void;
+  isPlaying: boolean;
+  beat: number;
+  bar: number;
+};
