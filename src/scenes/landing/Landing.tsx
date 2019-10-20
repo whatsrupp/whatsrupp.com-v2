eb7a0fc5ed28bd30725b845d25117e3e6b423900
whatsrupp <nickrupp95@gmail.com>
2019-10-20 19:06:11 +0100

    :recycle: refactor landing page to use reusable component

--- src/scenes/landing/Landing.tsx
+++ src/scenes/landing/Landing.tsx
@@ -5,44 +5,86 @@ import icons from "../../style/icons";
 import NathanFace from "./assets/images/NathanFace.png";
 import LoopsLogo from "./assets/images/LoopsLogo.png";
 import ReactPageScroller from "react-page-scroller";
+import LandGridLogo from "../landGrid/Logo";
+
+type RowProps = {
+  RowComponent: any;
+  link: string;
+  header: string;
+  text: string;
+  iconSrc?: string;
+  IconComponent?: any;
+};
 
 const Landing: React.FC = () => {
   const reactPageScroller = useRef();
 
+  const Row = ({
+    RowComponent,
+    link,
+    header,
+    text,
+    iconSrc,
+    IconComponent
+  }: RowProps) => {
+    const renderIcon = () => {
+      if (IconComponent) {
+        return <IconComponent />;
+      } else {
+        return <SC.RowPicture src={iconSrc} />;
+      }
+    };
+    return (
+      <SC.FullPage>
+        <RowComponent to={link}>
+          {renderIcon()}
+          <SC.Information>
+            <SC.RowHeader>{header}</SC.RowHeader>
+            <SC.RowText>{text}</SC.RowText>
+          </SC.Information>
+        </RowComponent>
+      </SC.FullPage>
+    );
+  };
+
   return (
     <SC.PageLayout>
       <ReactPageScroller ref={reactPageScroller}>
-        <SC.FullPage>
-          <SC.PortfolioRow to={"/portfolio"}>
-            <SC.RowPicture src={icons.palette} />
-            <SC.Information>
-              <SC.RowHeader>whatsrupp.com v 1.0.0</SC.RowHeader>
-              <SC.RowText>
-                My first attempt at a portfolio website back in 2017
-              </SC.RowText>
-            </SC.Information>
-          </SC.PortfolioRow>
-        </SC.FullPage>
-        <SC.FullPage>
-          <SC.SoundboardRow to={"/soundboard"}>
-            <SC.RowPicture src={NathanFace} />
-            <SC.Information>
-              <SC.RowHeader>King Soundboard</SC.RowHeader>
-              <SC.RowText>A Soundboard made to annoy my housemate</SC.RowText>
-            </SC.Information>
-          </SC.SoundboardRow>
-        </SC.FullPage>
-        <SC.FullPage>
-          <SC.LoopsRow to={"/loops"}>
-            <SC.RowPicture src={LoopsLogo} />
-            <SC.Information>
-              <SC.RowHeader>Loops and Cats</SC.RowHeader>>
-              <SC.RowText>
-                A Live Looping Recording Station - Currently Under Construction
-              </SC.RowText>
-            </SC.Information>
-          </SC.LoopsRow>
-        </SC.FullPage>
+        <Row
+          RowComponent={SC.PortfolioRow}
+          link={"/portfolio"}
+          iconSrc={icons.palette}
+          header={"whatsrupp.com v 1.0.0"}
+          text={"My first attempt at a portfolio website back in 2017"}
+        />
+
+        <Row
+          RowComponent={SC.SoundboardRow}
+          link={"/soundboard"}
+          iconSrc={NathanFace}
+          header={"King Soundboard"}
+          text={"A Soundboard made to annoy my housemate"}
+        />
+
+        <Row
+          RowComponent={SC.LoopsRow}
+          link={"/loops"}
+          iconSrc={LoopsLogo}
+          header={"Loops and Cats"}
+          text={
+            "A Live Looping Recording Station - Currently Under Construction"
+          }
+        />
+
+        <Row
+          RowComponent={SC.LandGridRow}
+          link={"/land-grid"}
+          IconComponent={LandGridLogo}
+          header={"Land Grid"}
+          text={
+            " An interactive chart to display the location and relative pricing of houses"
+          }
+        />
       </ReactPageScroller>
     </SC.PageLayout>
   );
