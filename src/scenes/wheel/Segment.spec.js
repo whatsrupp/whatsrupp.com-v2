16b7e6b3337e9e749e96199e36cb53cb9a1f433e
whatsrupp <nickrupp95@gmail.com>
2019-11-09 20:02:02 +0000

    create initial tests for the segment component

--- src/scenes/wheel/Segment.spec.js
+++ src/scenes/wheel/Segment.spec.js
@@ -3,12 +3,86 @@ import { render } from "@testing-library/react";
 import Segment from "./Segment";
 
 describe("Segment", () => {
-  it("returns a path", () => {
+  it("renders a path", () => {
     const container = render(
       <svg>
         <Segment />
       </svg>
     );
-    expect(container.container).toBeDefined();
+    const path = container.getByTestId("segment-path");
+    expect(path).toBeDefined();
+  });
+
+  it("renders a path with a definition", () => {
+    const container = render(
+      <svg>
+        <Segment />
+      </svg>
+    );
+    const path = container.getByTestId("segment-path");
+    const pathDefinition = path.getAttribute("d");
+    expect(pathDefinition).not.toBeNull();
+  });
+
+  it("renders the correct text", () => {
+    const container = render(
+      <svg>
+        <Segment text={"test text"} />
+      </svg>
+    );
+    const text = container.getByText("test text");
+
+    expect(text).toBeDefined();
+  });
+
+  describe("segment path definitions", () => {
+    it.each([
+      [
+        0,
+        180,
+        10,
+        0,
+        0,
+        0,
+        "M0,10 A10,10,0,0,0,0,-10 L0,0 A0,0,0,0,1,0,0 L0,10 z"
+      ]
+    ])(
+      "produces the expected path",
+      (
+        startAngle,
+        sweepAngle,
+        outerRadius,
+        innerRadius,
+        cx,
+        cy,
+        expectedPath
+      ) => {
+        const props = {
+          startAngle,
+          sweepAngle,
+          outerRadius,
+          innerRadius,
+          centreCoordinates: { cx, cy }
+        };
+
+        const container = render(
+          <svg>
+            <Segment {...props} />
+          </svg>
+        );
+
+        const removeWhitespaces = s => {
+          return s.replace(/\s/g, "");
+        };
+
+        const pathDefinition = container
+          .getByTestId("segment-path")
+          .getAttribute("d");
+
+        expect(removeWhitespaces(pathDefinition)).toEqual(
+          removeWhitespaces(expectedPath)
+        );
+      }
+    );
   });
 });
--- src/scenes/wheel/Segment.tsx
+++ src/scenes/wheel/Segment.tsx
@@ -1,9 +1,100 @@
 import React from "react";
 
-type SegmentProps = { test: string };
+type CartesianCoordinates = {
+  x: number;
+  y: number;
+};
 
-const Segment = ({ test }: SegmentProps) => {
-  return <path>{test}</path>;
+type PolarCoordinates = {
+  r: number;
+  theta: number;
+};
+
+interface ISegmentProperties {
+  text: string;
+}
+
+interface ISegmentDefinition {
+  sweepAngle: number;
+  startAngle: number;
+  outerRadius: number;
+  innerRadius: number;
+  centreCoordinates: CartesianCoordinates;
+  rotation: number;
+}
+
+type SegmentProps = ISegmentDefinition & ISegmentProperties;
+type GenerateSegmentCoordinates = ISegmentDefinition;
+
+const inRadians = (degrees: number) => {
+  return (degrees * Math.PI) / 180;
+};
+
+const inCartesianCoordinates = (
+  polarCoordinates: PolarCoordinates
+): CartesianCoordinates => {
+  const { r, theta } = polarCoordinates;
+  const x = r * Math.sin(theta);
+  const y = r * Math.cos(theta);
+  return { x: parseFloat(x.toFixed(4)), y: parseFloat(y.toFixed(4)) };
+};
+
+const Segment = (props: SegmentProps) => {
+  const {
+    text = "",
+    sweepAngle: sweepAngleDegrees = 180,
+    startAngle: startAngleDegrees = 0,
+    outerRadius = 10,
+    innerRadius = 0,
+    centreCoordinates = { x: 0, y: 0 },
+    rotation = 0
+  } = props;
+
+  const sweepAngle = inRadians(sweepAngleDegrees);
+  const startAngle = inRadians(startAngleDegrees);
+  const centreAngle = (sweepAngle - startAngle) / 2;
+  const endAngle = startAngle + sweepAngle;
+  const cx = centreCoordinates.x;
+  const cy = centreCoordinates.y;
+  const pi = Math.PI;
+  const r = innerRadius;
+  const R = outerRadius;
+  const centreRadius = r + (R - r) / 2;
+  const isBigArc = sweepAngleDegrees > 180;
+  const bigArcValue = isBigArc ? 1 : 0;
+
+  /*
+        Draw Order
+      1 ----->----- 2
+       \           /
+        \    c    /
+         \       /
+          4 -<- 3
+    */
+
+  const point1 = inCartesianCoordinates({ r: R, theta: startAngle });
+  const point2 = inCartesianCoordinates({ r: R, theta: endAngle });
+  const point3 = inCartesianCoordinates({ r, theta: endAngle });
+  const point4 = inCartesianCoordinates({ r, theta: startAngle });
+  const centre = inCartesianCoordinates({
+    r: centreRadius,
+    theta: centreAngle
+  });
+
+  const pathDefinition = `
+    M ${point1.x},${point1.y}
+    A ${R},${R},0,${bigArcValue},0,${point2.x},${point2.y}
+    L ${point3.x},${point3.y}
+    A ${r},${r},0,${bigArcValue},1,${point4.x},${point4.y}
+    L ${point1.x},${point1.y}
+    z`;
+
+  return (
+    <>
+      <path data-testid="segment-path" d={pathDefinition} />
+      <text>{text}</text>
+    </>
+  );
 };
 
 export default Segment;
