90a071cd352f0ff88d823f8eb17fb348a302bf32
whatsrupp <nickrupp95@gmail.com>
2019-11-09 20:02:29 +0000

    render segment for visual testing

--- src/scenes/wheel/index.tsx
+++ src/scenes/wheel/index.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 
 import * as SC from "./styled";
+import Segment from "./Segment";
 
 const Wheel: React.FC = () => {
   return (
     <SC.PageLayout>
       <SC.Header></SC.Header>
-      <SC.Body></SC.Body>
+      <SC.Body>
+        <svg height={100} width={100} viewBox={"-10 -10 20 20"}>
+          <Segment
+            sweepAngle={355}
+            startAngle={0}
+            outerRadius={10}
+            innerRadius={5}
+            centreCoordinates={{ x: 0, y: 0 }}
+            rotation={0}
+            text={"Hello"}
+          />
+        </svg>
+      </SC.Body>
       <SC.Footer></SC.Footer>
     </SC.PageLayout>
   );
