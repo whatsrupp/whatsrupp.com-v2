a58515346f857fa1b4ce9dd566919b76092a80f2
whatsrupp <nickrupp95@gmail.com>
2019-11-11 00:36:15 +0000

    integrate slider controls into the index page

--- src/scenes/wheel/index.tsx
+++ src/scenes/wheel/index.tsx
@@ -2,23 +2,49 @@ import React from "react";
 
 import * as SC from "./styled";
 import Segment from "./Segment";
+import { Slider, useSlider } from "./Slider";
 
 const Wheel: React.FC = () => {
+  const sweep = useSlider({ label: "Sweep", min: 0, max: 359 });
+  const startAngle = useSlider({ label: "Start Angle", min: 0, max: 359 });
+  const outerRadius = useSlider({ label: "Outer Radius", min: 0, max: 100 });
+  const innerRadius = useSlider({ label: "Inner Radius", min: 0, max: 95 });
+  const angularOffset = useSlider({
+    label: "Angular Offset",
+    min: 0,
+    max: 360
+  });
+
+  const canvasWidth = Math.max(innerRadius.value, outerRadius.value) * 2 + 5;
+  const canvasHeight = canvasWidth;
+
   return (
     <SC.PageLayout>
       <SC.Header></SC.Header>
+
       <SC.Body>
-        <svg height={100} width={100} viewBox={"-10 -10 20 20"}>
+        <svg
+          height={500}
+          width={500}
+          viewBox={`${-canvasWidth / 2} ${-canvasHeight /
+            2} ${canvasWidth} ${canvasHeight}`}
+        >
           <Segment
-            sweepAngle={355}
-            startAngle={0}
-            outerRadius={10}
-            innerRadius={5}
-            centreCoordinates={{ x: 0, y: 0 }}
-            rotation={0}
-            text={"Hello"}
+            sweepAngle={sweep.value}
+            startAngle={startAngle.value}
+            outerRadius={outerRadius.value}
+            innerRadius={innerRadius.value}
+            angularOffset={angularOffset.value}
+            radialOffset={0}
+            text={""}
           />
         </svg>
+
+        <Slider {...sweep} />
+        <Slider {...startAngle} />
+        <Slider {...outerRadius} />
+        <Slider {...innerRadius} />
+        <Slider {...angularOffset} />
       </SC.Body>
       <SC.Footer></SC.Footer>
     </SC.PageLayout>
