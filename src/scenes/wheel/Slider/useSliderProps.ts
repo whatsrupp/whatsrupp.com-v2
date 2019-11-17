9accbb457666ffa20a30d593e7096dac72838715
whatsrupp <nickrupp95@gmail.com>
2019-11-17 20:58:41 +0000

    remove unused variables

--- src/scenes/wheel/Slider/useSliderProps.ts
+++ src/scenes/wheel/Slider/useSliderProps.ts
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import { debounce } from "lodash";
 import { slideEvent } from "./analytics";
 type UseSliderProps = {
