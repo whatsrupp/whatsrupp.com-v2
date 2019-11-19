bb7e5c1d670697d3cca7d46b8b721bc1190a5d12
whatsrupp <nickrupp95@gmail.com>
2019-11-19 17:55:41 +0000

    create a hook to generate chord callbacks

---
+++ src/scenes/wheel/Wheel/useChordPlayer.ts
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { getChordFrequencies } from "./musicHelpers";
+
+export const useChordPlayer = () => {
+  const [audioContext, setAudioContext] = useState(null);
+
+  const constructChordPlayCallback = (key: any, mode: string): Function => {
+    const callback = () => {
+      let currentAudioContext = audioContext;
+      if (!currentAudioContext) {
+        currentAudioContext = new AudioContext();
+        setAudioContext(currentAudioContext);
+      }
+
+      const frequencies = getChordFrequencies(key, 4, mode);
+      const currentTime = currentAudioContext.currentTime;
+
+      frequencies.forEach((frequency, i) => {
+        let oscillator = currentAudioContext.createOscillator();
+        oscillator.frequency.value = frequency;
+        const startTime = currentTime + i / 5;
+        const stopTime = currentTime + 1;
+        oscillator.start(startTime);
+        oscillator.stop(stopTime);
+        oscillator.connect(currentAudioContext.destination);
+      });
+    };
+    return callback;
+  };
+
+  return constructChordPlayCallback;
+};
+
+export default useChordPlayer;
