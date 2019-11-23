ef893ce6913f97b8018470c423d0f2973e63e04a
whatsrupp <nickrupp95@gmail.com>
2019-11-23 17:44:45 +0000

    add extra event constants

--- src/services/analytics/constants.ts
+++ src/services/analytics/constants.ts
@@ -1,8 +1,10 @@
 export const actions = {
   MOVE_SLIDER_INPUT: "move-slider-input",
-  CLICK_SEGMENT: "click-segment"
+  CLICK_SEGMENT: "click-segment",
+  CLICK_HOME_BUTTON: "click-home-button"
 };
 
 export const categories = {
-  INTERACTIONS: "interactions"
+  INTERACTIONS: "interactions",
+  NAVIGATION: "navigation"
 };
--- src/style/colours.ts
+++ src/style/colours.ts
@@ -1,11 +1,9 @@
-export const red : string = '#E27D60'
-export const teal : string = '#E27D60'
-export const orange : string = '#E27D60'
-export const pink : string = '#E27D60'
-export const green : string = '#E27D60'
-export const softBlack : string= '#1f304a';
-export const white : string = '#FFFFFF';
-
-
-export const deepPurple : string = '#222036'
-export const  purple : string = '#403d66'
+export const red: string = "#E27D60";
+export const teal: string = "#E27D60";
+export const orange: string = "#E27D60";
+export const pink: string = "#E27D60";
+export const green: string = "#E27D60";
+export const softBlack: string = "#1f304a";
+export const white: string = "#FFFFFF";
+export const deepPurple: string = "#222036";
+export const purple: string = "#403d66";
