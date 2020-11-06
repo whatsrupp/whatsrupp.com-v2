7ecc984c41e73db0d5cfa1de001934047e6e3070
whatsrupp <nickrupp95@gmail.com>
2020-11-06 14:36:30 +0000

    add icons

---
+++ src/scenes/demolition/icons/index.ts
@@ -0,0 +1,26 @@
+import coding from "./Coding.png";
+import drawing from "./Drawing.png";
+import grid from "./Grid.png";
+import loops from "./Loops.png";
+import mannequin from "./Mannequin.png";
+import nathan from "./Nathan.png";
+import photography from "./Photography.png";
+import segment from "./Segment.png";
+import wheel from "./Wheel.png";
+import wrench from "./Wrench.png";
+import writing from "./Writing.png";
+
+const images = {
+  coding,
+  drawing,
+  grid,
+  loops,
+  mannequin,
+  nathan,
+  photography,
+  segment,
+  wheel,
+  wrench,
+  writing
+};
+export default images;
