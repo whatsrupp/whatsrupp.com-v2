7dfb9614e13e2ece876e2f05edd229c713abb8d3
whatsrupp <nickrupp95@gmail.com>
2020-11-07 18:45:34 +0000

    add some descriptions and add the concept of external links

--- src/scenes/demolition/projects.ts
+++ src/scenes/demolition/projects.ts
@@ -6,6 +6,7 @@ export type Project = {
   icon: string;
   title: string;
   subtitle: string;
+  isExternal: Boolean;
   link: string;
   description: string;
 };
@@ -30,6 +31,7 @@ const projects = [
     icon: icons.nathan,
     title: "King Soundboard",
     link: routes.SOUNDBOARD,
+    isExternal: false,
     subtitle: "A soundboard made to annoy my housemate",
     description: `The year was 2018, the airport was London Gatwick. Some uni friends and I had just been told about a 4 hour delay. Luckily, I had VS Code to hand and used the time to whip up a small soundboard of my friend's common expressions. The annoyance it has created over the last few years has been sensational.`
   },
@@ -38,7 +40,7 @@ const projects = [
     icon: icons.grid,
     title: "Land Grid",
     link: routes.LAND_GRID,
-
+    isExternal: false,
     subtitle:
       "An interactive chart to display the location and relative pricing of houses",
     description:
@@ -48,7 +50,7 @@ const projects = [
     id: projectIds.portfolio,
     icon: icons.drawing,
     link: routes.PORTFOLIO,
-
+    isExternal: false,
     title: "whatsrupp.com v1.0.0",
     subtitle: "My first design for a portfolio back in 2017",
     description:
@@ -58,6 +60,7 @@ const projects = [
     id: projectIds.coding,
     icon: icons.coding,
     link: "https://github.com/whatsrupp",
+    isExternal: true,
     title: "Coding",
     subtitle:
       "A link to my github if you want to look at the source code for this website! Steal Away!",
@@ -68,9 +71,10 @@ const projects = [
     id: projectIds.loops,
     icon: icons.loops,
     link: routes.LOOPS,
-
+    isExternal: false,
     title: "Loops and Cats",
-    subtitle: "A live looping recording station",
+    subtitle:
+      "A live looping recording station. Beatbox your boots and cats and make a fresh track.",
     description:
       "This was my first ever collaborative coding project. 3 friends and I wanted to rustle up something to quickly brainstorm musical ideas. A loop station is hardly original, but one that's quick and accessible in a browser has its perks. It turned out that it was very hard to keep audio playback for multiple tracks in sync. The original MVP code is here https://github.com/whatsrupp/loops-and-cats it was a single page javascript application written in vanilla js. The version on this website is my rewrite of it using react. "
   },
@@ -78,9 +82,10 @@ const projects = [
     id: projectIds.wheel,
     icon: icons.wheel,
     link: routes.CAMELOT_WHEEL,
-
+    isExternal: false,
     title: "Camelot Wheel",
-    subtitle: "A musical SVG",
+    subtitle:
+      "Also known as the circle of fifths. An Interactive musical SVG to try out chord progressions.",
     description:
       "In 2019 I helped my friend Jack on a project called play-thru (https://github.com/jackbittiner/play-thru). Given a song from the spotify API it recommends other songs that are compatible with the key and the tempo of the song. A core concept behind this automatic mixing is something called the Camelot Wheel, also known as the circle of fifths. All musical keys adjacent to the block you've chosen in the wheel will be musically compatible."
   },
@@ -88,28 +93,28 @@ const projects = [
     id: projectIds.segment,
     icon: icons.segment,
     link: routes.SEGMENT,
-
+    isExternal: false,
     title: "Segment",
     subtitle: "A radial SVG builder for react",
     description:
       "In 2019 I helped my friend Jack on a project called play-thru (https://github.com/jackbittiner/play-thru). A core part of the UI required the ability to easily create clickable circular components. SVGs work perfectly in the situation you have weirdly shaped dom elements that you need to be able to click. This react component makes it easy to build segments based on a number of key parameters."
   },
-  {
-    id: projectIds.engineering,
-    icon: icons.wrench,
-    link: routes.SOUNDBOARD,
+  //   {
+  //     id: projectIds.engineering,
+  //     icon: icons.wrench,
+  //     link: routes.SOUNDBOARD,
 
-    title: "Engineering",
-    subtitle: "A collection of more technical mechanical engineering projects",
-    description:
-      "Before becoming a developer I studied Engineering at university. I ended up really enjoyed control systems, where you use code to control physical things. This is an archive of the more visual things we had to make."
-  },
+  //     title: "Engineering",
+  //     subtitle: "A collection of more technical mechanical engineering projects",
+  //     description:
+  //       "Before becoming a developer I studied Engineering at university. I ended up really enjoyed control systems, where you use code to control physical things. This is an archive of the more visual things we had to make."
+  //   },
   {
     id: projectIds.writing,
     icon: icons.writing,
-    link: routes.SOUNDBOARD,
-
+    link: "https://medium.com/@nickrupp95",
     title: "Engineering Blog",
+    isExternal: true,
     subtitle:
       "Man Vs. Code a blog about what it's like to do a coding bootcamp",
     description:
@@ -118,23 +123,24 @@ const projects = [
   {
     id: projectIds.photography,
     icon: icons.photography,
-    link: routes.SOUNDBOARD,
-
-    title: "Nick Pics",
-    subtitle: "Night time and Nature",
+    link: "https://www.instagram.com/nick.rupp/?hl=en",
+    isExternal: true,
+    title: "Photography",
+    subtitle:
+      "Nick Pics. I'm a sucker for long exposures and an animal. I take portaits on the side.",
     description:
       "I think I like photography because I like collecting things. Eiffel Tower?. Snap. Collected it. Bird? Snap. Collected it. So satisfying."
-  },
-  {
-    id: projectIds.about,
-    icon: icons.mannequin,
-    link: routes.SOUNDBOARD,
-
-    title: "About Me",
-    subtitle: "Proceed with caution.",
-    description:
-      "Realistically, a ballpit of projects just isn't going to tell you much about me. Here's a bit more."
   }
+  //   {
+  //     id: projectIds.about,
+  //     icon: icons.mannequin,
+  //     link: routes.SOUNDBOARD,
+
+  //     title: "About Me",
+  //     subtitle: "Proceed with caution.",
+  //     description:
+  //       "Realistically, a ballpit of projects just isn't going to tell you much about me. Here's a bit more."
+  //   }
 ];
 
 export const projectsMap = new Map(
