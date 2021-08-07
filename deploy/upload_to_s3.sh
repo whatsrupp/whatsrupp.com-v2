5b45791dac4a8c61627b122564cc39063b8b1bfc
whatsrupp <nickrupp95@gmail.com>
2021-08-07 12:38:05 +0100

    :construction_worker: add tagging and master branch check to deploy script

--- deploy/upload_to_s3.sh
+++ deploy/upload_to_s3.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+if [[ "$BRANCH" != "master" ]]; then
+  echo 'can only deploy on master branch';
+  exit 1;
+fi
+
 
 ACCOUNT_ID=$(aws sts get-caller-identity | jq -r .Account)
 
 yarn build
 
 BUCKET_NAME=whatsrupp-production
-aws s3 sync ../build s3://$BUCKET_NAME --delete
+aws s3 sync $SCRIPT_DIR/../build s3://$BUCKET_NAME --delete
 
+cd $SCRIPT_DIR/..
+npm version patch
+new_version=$(jq .version package.json)
+git add package.json
+git commit -m "bump version to ${new_version} [ci-skip]"
+git push origin master
