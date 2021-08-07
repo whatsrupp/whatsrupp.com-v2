331b42b445b1a18b2f6e5c6cd943ec1ec1d3819b
whatsrupp <nickrupp95@gmail.com>
2021-08-07 12:30:51 +0100

    :recycle: rename variables and remove named functional component

--- src/scenes/loops/Loops.tsx
+++ src/scenes/loops/Loops.tsx
@@ -6,30 +6,28 @@ import Metronome from "./Metronome";
 import MetronomeButton from "./MetronomeButton";
 import Tracks from "./Tracks";
 const Loops: React.FC = () => {
-  const [hasInteracted, setHasInteracted] = useState(false);
+  const [userHasInteracted, setUserHasInteracted] = useState(false);
 
-  /** We can't render the page here straight away because if we enter the loops page directly the user needs to interact with the page before we create an audio context (used in the <Metronome /> component)
+  /** We can't render the main page here straight away because if we enter the loops page directly the user needs to interact with the page before we create an audio context (used in the <Metronome /> component)
    * This is due to browser's autoplay policies eg chromes:
    * https://developer.chrome.com/blog/autoplay/#webaudio
    */
-  const AudioContextChecker: React.FC = () => (
-    <SC.PageLayout>
-      <SC.Header />
+  if (!userHasInteracted) {
+    return (
+      <SC.PageLayout>
+        <SC.Header />
 
-      <SC.Body>
-        <SC.GetStartedButton
-          onClick={event => {
-            setHasInteracted(true);
-          }}
-        >
-          Get Looping
-        </SC.GetStartedButton>
-      </SC.Body>
-    </SC.PageLayout>
-  );
-
-  if (!hasInteracted) {
-    return <AudioContextChecker></AudioContextChecker>;
+        <SC.Body>
+          <SC.GetStartedButton
+            onClick={event => {
+              setUserHasInteracted(true);
+            }}
+          >
+            Get Looping
+          </SC.GetStartedButton>
+        </SC.Body>
+      </SC.PageLayout>
+    );
   } else {
     return (
       <Metronome>
