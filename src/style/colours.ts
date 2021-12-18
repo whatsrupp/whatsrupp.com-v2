8c1fbb9730d9209b9d7f1de38f02da6fe4d8f507
whatsrupp <nickrupp95@gmail.com>
2021-12-18 20:42:06 +0000

    feat: add new darker purple colour

--- src/style/colours.ts
+++ src/style/colours.ts
@@ -1,3 +1,5 @@
+import { darken, lighten } from "polished";
+
 export const red: string = "#E27D60";
 export const teal: string = "#E27D60";
 export const orange: string = "#E27D60";
@@ -7,3 +9,5 @@ export const softBlack: string = "#1f304a";
 export const white: string = "#FFFFFF";
 export const deepPurple: string = "#222036";
 export const purple: string = "#403d66";
+export const darkPurple: string = darken(0.2, purple);
+export const lightPurple: string = lighten(0.2, purple);
